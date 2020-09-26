7b30309048e7b3323e0a07f129cb66e7adffba97
tomohiro3 <tomohiro3jp@gmail.com>
2020-09-27 00:26:34 +0900

    Deleted node-sass which is deprecated and fixed the issue scss doesn't get loaded at the dev server start

--- next.config.js
+++ next.config.js
@@ -1,10 +1,16 @@
 const path = require('path')
-const withSass = require('@zeit/next-sass')
+// const withSass = require('@zeit/next-sass')
 
-module.exports = withSass({
-  cssModules: true,
-  includePaths: [path.join(__dirname, 'styles')],
-  cssLoaderOptions: {
-    importLoaders: 2,
+module.exports = {
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
   },
-})
+};
+// This is an old way to configure sass, I guess, because it requies node-sass which is deprecated
+// module.exports = withSass({
+//   cssModules: true,
+//   includePaths: [path.join(__dirname, 'styles')],
+//   cssLoaderOptions: {
+//     importLoaders: 2,
+//   },
+// })
