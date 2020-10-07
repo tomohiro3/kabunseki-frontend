f0e855262c0b27b2fdc94bc0e7a42352022c0a8a
tomohiro3 <tomohiro3jp@gmail.com>
2020-10-08 01:27:46 +0900

    Render the page component with react-axe in local environment

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,18 +2,26 @@
 // This is like App.js of create-react-app
 // Global setting will be done in here.
 // You can use this component for error hundle HOC, redux provider and so on.
-import '../styles/globals.scss'
-import Layout from "../components/templates/Layout";
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+if (process.env.CHECK_AXE) {
+  import('react-axe').then((axe) => {
+    axe.default(React, ReactDOM, 1000);
+  });
+}
+
+import '../styles/globals.scss';
+import Layout from '../components/templates/Layout';
 
 function App({ Component, pageProps }) {
-  return(
+  return (
     <>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     </>
-  )
+  );
 }
 
-
-export default App
+export default App;
