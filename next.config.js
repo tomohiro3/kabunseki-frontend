bae41238664b1c96def114f899d197c2019b1593
tomohiro3 <tomohiro3jp@gmail.com>
2020-10-08 01:26:52 +0900

    Added an evironmental variable for react-axe

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
-const path = require('path')
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path');
+
 // const withSass = require('@zeit/next-sass')
 
 module.exports = {
   sassOptions: {
-    includePaths: [path.join(__dirname, 'styles')],
+    includePaths: [path.join(__dirname, 'styles')]
   },
+  env: {
+    CHECK_AXE: process.env.CHECK_AXE
+  }
 };
 // This is an old way to configure sass, I guess, because it requies node-sass which is deprecated
 // module.exports = withSass({
